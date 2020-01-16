bb1df1e0d456c18530cb40b6777749fe068cf745
Greg <greg@mail.com>
2020-01-16 11:36:42 +0100

    style css list

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ var List = [
 {
 	label:"Pomme", status:"suppr", important:"1"}];
 
-
+	var i = 1;
 	for (var valeur of List) {
 		var valueList = document.querySelector(".list");
-		valueList.insertAdjacentHTML('afterbegin','<li>'+ valeur.label +'</li>');
+		valueList.insertAdjacentHTML('beforebegin','<li class="listElement"><span class="numberList">'+ i +'</span>'+ valeur.label +'</li>');
 		console.log("valueList");
+		var counter = i++;
 	};
+
+	var valueCounter= document.querySelector(".number");
+	valueCounter.innerHTML = counter;
