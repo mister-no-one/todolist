c0466d6ef5b32bda85eaaf38667156a9dfdaada5
Greg <greg@mail.com>
2020-01-16 14:25:40 +0100

    step2 form add new item ok

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var List = [
+var list = [
 {
 	label:"Ananas", status:"suppr", important:"1"
 },
@@ -8,9 +8,14 @@ var List = [
 {
 	label:"Pomme", status:"suppr", important:"1"}];
 
+
+	var valueList = document.querySelector(".list");
 	var i = 1;
-	for (var valeur of List) {
-		var valueList = document.querySelector(".list");
+
+
+
+	for (var valeur of list) {
+		
 		valueList.insertAdjacentHTML('beforebegin','<li class="listElement"><span class="numberList">'+ i +'</span>'+ valeur.label +'</li>');
 		console.log("valueList");
 		var counter = i++;
@@ -21,11 +26,7 @@ var List = [
 
 	
 
-
-
-
-
-	var formItem = document.querySelector(".submit");
+	var formItem = document.querySelector(".form");
 	formItem.addEventListener("submit",function(e){
 		e.preventDefault();
 		processAdding();
@@ -34,9 +35,19 @@ var List = [
 	console.log(formItem);
 
 	function processAdding(){
+
 		var labelItem = document.querySelector(".item-label");
-		var value = labelItem.value;
-		console.log(labelItem);
+		var newItemValue = labelItem.value;
 
+	// ITEM ET PUSH INTO LIST
+	var newItem = {};
+	newItem.label = newItemValue;
+	var countItem = list.push(newItem);
 
-	}
+	// VIEW PROCESSING
+	valueList.insertAdjacentHTML('beforebegin','<li class="listElement"><span class="numberList">'+ countItem +'</span>'+ newItem.label +'</li>');
+
+	// UPDATE TOTAL ITEM
+	var valueCounter= document.querySelector(".number");
+	valueCounter.innerHTML = countItem;
+}
